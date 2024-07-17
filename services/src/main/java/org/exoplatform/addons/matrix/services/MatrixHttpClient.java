4b5fd33783435c40375c6e5ef43c1816f442bb5f
Ali Hamdi <ahamdi@exoplatform.com>
2024-07-17 22:10:42 +0100

    task: a created user will have an account on Matrix - EXO-72115

--- services/src/main/java/org/exoplatform/addons/matrix/services/MatrixHttpClient.java
+++ services/src/main/java/org/exoplatform/addons/matrix/services/MatrixHttpClient.java
@@ -1,17 +1,21 @@
 package org.exoplatform.addons.matrix.services;
 
+import org.apache.commons.codec.digest.HmacUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.User;
 import org.exoplatform.ws.frameworks.json.impl.JsonException;
 import org.exoplatform.ws.frameworks.json.impl.JsonGeneratorImpl;
+import org.exoplatform.ws.frameworks.json.value.JsonValue;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class MatrixHttpClient {
@@ -28,7 +32,8 @@ public class MatrixHttpClient {
 
   private static final String AUTHORIZATION                 = "Authorization";
 
-  private static final String SERVER_NAME                   = "exo.matrix.server.name";
+  public static final String SERVER_NAME                   = "exo.matrix.server.name";
+  private static final String SHARED_SECRET_REGISTRATION = "exo.matrix.shared_secret_registration";
 
   private MatrixHttpClient() {
   }
@@ -72,7 +77,7 @@ public class MatrixHttpClient {
         String roomId = jsonGenerator.createJsonObjectFromString(response.body()).getElement("room_id").getStringValue();
         return roomId.substring(0, roomId.indexOf(":" + PropertyManager.getProperty(SERVER_NAME)));
       } else {
-        LOG.error("Error creating a team, Mattermost server returned HTTP {} error {}", response.statusCode(), response.body());
+        LOG.error("Error creating a team, Matrix server returned HTTP {} error {}", String.valueOf(response.statusCode()), response.body());
         return null;
       }
     } catch (Exception e) {
@@ -95,11 +100,19 @@ public class MatrixHttpClient {
   }
   protected static HttpResponse<String> sendHttpPostRequest(String url, String token, String contentAsJson) throws IOException, InterruptedException {
     HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .header(AUTHORIZATION, BEARER + token)
-            .POST(HttpRequest.BodyPublishers.ofString(contentAsJson))
-            .build();
+    HttpRequest request;
+    if(StringUtils.isNotBlank(token)) {
+      request = HttpRequest.newBuilder()
+              .uri(URI.create(url))
+              .header(AUTHORIZATION, BEARER + token)
+              .POST(HttpRequest.BodyPublishers.ofString(contentAsJson))
+              .build();
+    } else {
+      request = HttpRequest.newBuilder()
+              .uri(URI.create(url))
+              .POST(HttpRequest.BodyPublishers.ofString(contentAsJson))
+              .build();
+    }
     return client.send(request, HttpResponse.BodyHandlers.ofString());
   }
 
@@ -116,4 +129,110 @@ public class MatrixHttpClient {
   public static void sendInvitationToMembers(ArrayList<String> strings, String matrixRoomId) {
 
   }
+
+  public static String createUserAccount(User user) {
+    if (StringUtils.isBlank(PropertyManager.getProperty(MATRIX_SERVER_URL))) {
+      throw new IllegalArgumentException(MATRIX_SERVER_URL_IS_REQUIRED);
+    }
+
+    String nonce = getRegistrationNonce();
+    String url = PropertyManager.getProperty(MATRIX_SERVER_URL) + "/_synapse/admin/v1/register";
+    String hmac = hmacUserProperties(nonce, user.getUserName(), user.getUserName(), false);
+
+    String payload = """
+        {
+           "nonce": "%s",
+           "username": "%s",PUT /_synapse/admin/v2/users/<user_id>
+           "displayname": "%s",
+           "password": "%s",
+           "admin": false,
+           "mac": "%s"
+          }
+        """.formatted(nonce, user.getUserName(), user.getDisplayName(), user.getUserName(), hmac);
+
+    try {
+      String token = PropertyManager.getProperty(MATRIX_ACCESS_TOKEN);
+      HttpResponse<String> response = sendHttpPostRequest(url, token, payload);
+      if(response.statusCode() >= 200 && response.statusCode() < 300) {
+        JsonGeneratorImpl jsonGenerator = new JsonGeneratorImpl();
+        JsonValue userAccount = jsonGenerator.createJsonObjectFromString(response.body());
+        return userAccount.getElement("user_id").getStringValue();
+      } else {
+        LOG.error("Error creating a user account, Matrix server returned HTTP {} error {}", String.valueOf(response.statusCode()), response.body());
+        return null;
+      }
+    } catch (Exception e) {
+      LOG.error("Could not create a user account on Matrix", e);
+      return null;
+    }
+  }
+
+  public static String saveUserAccount(User user) {
+    if (StringUtils.isBlank(PropertyManager.getProperty(MATRIX_SERVER_URL))) {
+      throw new IllegalArgumentException(MATRIX_SERVER_URL_IS_REQUIRED);
+    }
+
+    String url = PropertyManager.getProperty(MATRIX_SERVER_URL) + "/_synapse/admin/v2/users/" + "@" + user.getUserName().toLowerCase() + ":" + PropertyManager.getProperty(SERVER_NAME);
+
+    String payload = """
+        {
+         "password": "%s",
+         "logout_devices": false,
+         "displayname": "%s",
+         "threepids": [
+             {
+                 "medium": "email",
+                 "address": "%s"
+             }
+         ],
+         "admin": false,
+         "deactivated": %s,
+         "user_type": null,
+         "locked": false
+       }
+        """.formatted(user.getUserName().toLowerCase(), user.getDisplayName(), user.getEmail(), String.valueOf(user.isEnabled()));
+
+    try {
+      String token = PropertyManager.getProperty(MATRIX_ACCESS_TOKEN);
+      HttpResponse<String> response = sendHttpPutRequest(url, token, payload);
+      if(response.statusCode() >= 200 && response.statusCode() < 300) {
+        JsonGeneratorImpl jsonGenerator = new JsonGeneratorImpl();
+        JsonValue userAccount = jsonGenerator.createJsonObjectFromString(response.body());
+        return userAccount.getElement("name").getStringValue();
+      } else {
+        LOG.error("Error creating a user account, Matrix server returned HTTP {} error {}", String.valueOf(response.statusCode()), response.body());
+        return null;
+      }
+    } catch (Exception e) {
+      LOG.error("Could not create a user account on Matrix", e);
+      return null;
+    }
+  }
+
+
+  private static String hmacUserProperties(String nonce, String userName, String password, boolean isAdmin) {
+    String userProperties = nonce + "\0" + userName + "\0" + password + "\0" + (isAdmin? "admin" : "notadmin");
+    return HmacUtils.hmacSha1Hex(PropertyManager.getProperty(SHARED_SECRET_REGISTRATION), userProperties);
+  }
+
+  private static String getRegistrationNonce() {
+    String url = PropertyManager.getProperty(MATRIX_SERVER_URL) + "/_synapse/admin/v1/register";
+    try {
+      String token = PropertyManager.getProperty(MATRIX_ACCESS_TOKEN);
+      HttpResponse<String> response = sendHttpGetRequest(url, token);
+      if(response.statusCode() >= 200 && response.statusCode() < 300) {
+        JsonGeneratorImpl jsonGenerator = new JsonGeneratorImpl();
+        JsonValue jsonResponse = jsonGenerator.createJsonObjectFromString(response.body());
+        return jsonResponse.getElement("nonce").getStringValue();
+      } else {
+        LOG.error("Error getting Nonce, Matrix server returned HTTP {} error {}", String.valueOf(response.statusCode()), response.body());
+        return null;
+      }
+    } catch (Exception e) {
+      LOG.error("Could not get the nonce on Matrix", e);
+      return null;
+    }
+  }
+
+
 }
--- webapp/src/main/webapp/WEB-INF/jsp/matrix.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/matrix.jsp
@@ -1,11 +1,14 @@
+<%@page import="org.exoplatform.commons.utils.PropertyManager"%>
+
 <div class="VuetifyApp">
   <div data-app="true"
     class="v-application v-application--is-ltr theme--light"
     id="matrixChatButton">
     <script type="text/javascript">
       require(['PORTLET/matrix/Matrix'], app => {
+        const serverName = '<%= PropertyManager.getProperty(org.exoplatform.addons.matrix.services.MatrixHttpClient.SERVER_NAME)%>';
         if(eXo.env.portal.spaceId) {
-          fetch('/matrix/rest/matrix?spaceId='+eXo.env.portal.spaceId, {
+          fetch('/portal/rest/matrix?spaceId='+eXo.env.portal.spaceId, {
             method: 'GET',
             credentials: 'include',
           }).then(respMatrix => {
@@ -18,7 +21,7 @@
         	  if(respMatrixText) {
         		  console.log(respMatrixText);
         		  if(respMatrixText) {
-        		    app.init(respMatrixText);
+        		    app.init(respMatrixText, serverName);
 			        }
             }
           });
